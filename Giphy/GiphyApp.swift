2f3ce99280527d96e54bf84cd48f2ca6c8e17c46
Kadir Ince <kadirincedev@gmail.com>
2020-09-28 12:16:22 +0300

    Initial Commit

---
+++ Giphy/GiphyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiphyApp.swift
+//  Giphy
+//
+//  Created by Kadir Ince on 28.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct GiphyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
